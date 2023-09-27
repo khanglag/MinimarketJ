8a0c979cecdbfafbf6a30782d057678c0109b52c
Chị Tươi <nguyentranyennhi.2108@gmail.com>
2023-09-27 17:07:51 +0700

    up

--- src/main/java/DAO/KhachHangDAO.java
+++ src/main/java/DAO/KhachHangDAO.java
@@ -4,10 +4,35 @@
  */
 package DAO;
 
+import ConnectDB.ConnectDB;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author khang
  */
 public class KhachHangDAO {
+    
+    public KhachHangDAO{
+    
+}
+    ConnectDB connectDB;
+    ArrayList<Khachhang> dskh = new ArrayList<Khachhang>();
+    
+    public ArrayList<Khachhang> readDB(){
+        connectDB = new ConnectDB();
+        try{
+            String qry = "SELECT *FROM KHACHHANG WHERE TONTAI =1";
+            ResultSet rset = connectDB.sqlQuery(qry);
+            if(rset != null){
+                while(rset.next()){
+                    Khachhang kh = new Khachhang(rset.getNString("Ma_kh")),
+                                            rset.getNString()
+                }
+            }
+        }
+    }
+    
     
 }
